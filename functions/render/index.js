4e32068ef56e9f3fc7dbbd57ae1113e0b6456599
Henrik Fricke <henrikfricke@web.de>
2021-04-23 17:37:02 +0200

    feat: hacker news clone

--- functions/render/index.js
+++ functions/render/index.js
@@ -1,16 +1,17 @@
+const Parser = require('rss-parser');
+const nunjucks = require('nunjucks');
+const fs = require('fs');
+const path = require('path');
+
+const parser = new Parser();
+nunjucks.configure({ autoescape: true });
+
 exports.handler = async () => {
+  const feed = await parser.parseURL('https://news.ycombinator.com/rss');
+  const html = fs.readFileSync(path.join(__dirname, './html.njk'), 'utf-8');
+
   return {
     statusCode: 200,
-    body: `
-      <html>
-        <head>
-        <link rel="stylesheet" href="styles.css">
-        </head>
-        <body>
-          <h1>Hello from the server :)</h1>
-          <p>${new Date().toISOString()}</p>
-        </body>
-      </html>
-    `
+    body: nunjucks.renderString(html, { items: feed.items })
   }
 }
