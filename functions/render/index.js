9f527a82bde6148082c926b485df312b59bed7f3
Henrik Fricke <henrikfricke@web.de>
2021-04-24 14:00:40 +0200

    feat(css): make the world a bit shiny

--- functions/render/index.js
+++ functions/render/index.js
@@ -1,10 +1,12 @@
 const Parser = require('rss-parser');
 const nunjucks = require('nunjucks');
+const dateFilter = require('nunjucks-date-filter');
 const fs = require('fs');
 const path = require('path');
 
 const parser = new Parser();
 nunjucks.configure({ autoescape: true });
+dateFilter.install();
 
 exports.handler = async () => {
   const feed = await parser.parseURL('https://news.ycombinator.com/rss');
@@ -12,6 +14,9 @@ exports.handler = async () => {
 
   return {
     statusCode: 200,
-    body: nunjucks.renderString(html, { items: feed.items })
+    body: nunjucks.renderString(html, { items: feed.items }),
+    headers: {
+      'content-type': 'text/html; charset=utf-8'
+    }
   }
 }
